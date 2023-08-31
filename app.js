437860f74d1e97d30ab76901ca7cfca27af5e318
Gabriel <gabi_bueno92@hotmail.com>
2023-08-31 17:52:20 +1000

    template added

---
+++ app.js
@@ -0,0 +1,29 @@
+/* const express = require('express');
+const app = express();
+const port = process.env.PORT ?? 3000;
+
+app.use(express.static('public'))
+
+app.get('*', (req, res) => {
+    res.redirect('/');
+})
+
+app.listen(port, () => {
+    console.log(`App listening on port ${port}`);
+})
+ */
+const express = require('express')
+const cors = require('cors')
+const { createProxyMiddleware } = require('http-proxy-middleware')
+
+const app = express()
+app.use(cors())
+app.use(createProxyMiddleware({
+  router: (req) => new URL(req.path.substring(1)),
+  pathRewrite: (path, req) => (new URL(req.path.substring(1))).pathname,
+  changeOrigin: true,
+  logger: console
+}))
+
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => console.log(`listening on ${PORT}`));
